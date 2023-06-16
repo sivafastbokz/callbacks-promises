a5cb1359b621086cc671c0837c9b05c190801508
sivafastbokz <sivaharshanfastbokz@gmail.com>
2023-06-16 14:33:20 +0530

    logical funtion pratice

--- callbacks&promises.js
+++ callbacks&promises.js
@@ -193,54 +193,306 @@
 
 
 //settimeout and setinterval
-function mytransaction(accountholder){
-     setTimeout(() => {
-        console.log('your transactions being here')
-     }, 1000);
-    accountholder(chooseaccount)
+// function mytransaction(accountholder){
+//      setTimeout(() => {
+//         console.log('your transactions being here')
+//      }, 1000);
+//     accountholder(chooseaccount)
     
-}
+// }
 
-function message(){
-    setTimeout(() => {
-        console.log('thank you visit again')
-    }, 16000);
-}
-
-
-function withdraw(withdrewmsg){
-   setTimeout(() => {
-      console.log('withdraw successfull')
-   }, 13000);
-    withdrewmsg()
-}
+// function message(){
+//     setTimeout(() => {
+//         console.log('thank you visit again')
+//     }, 16000);
+// }
 
 
-function atmpin(withdrawtheamount){
-    setTimeout(() => {
-        console.log('enter the pin')
-    }, 10000);
-    withdrawtheamount(message)
-}
+// function withdraw(withdrewmsg){
+//    setTimeout(() => {
+//       console.log('withdraw successfull')
+//    }, 13000);
+//     withdrewmsg()
+// }
 
 
-function chooseaccount(enterthepin){
-    setTimeout(() => {
-        console.log('sure you want to choose the savings account?')
-    }, 7000);
-    enterthepin(withdraw)
-}
-
-function accountholder(choosetheaccount){
-    setTimeout(() => {
-        console.log('welcome bob')
-    }, 4000);
-    choosetheaccount(atmpin)
-}
-
-mytransaction(accountholder)
+// function atmpin(withdrawtheamount){
+//     setTimeout(() => {
+//         console.log('enter the pin')
+//     }, 10000);
+//     withdrawtheamount(message)
+// }
 
 
+// function chooseaccount(enterthepin){
+//     setTimeout(() => {
+//         console.log('sure you want to choose the savings account?')
+//     }, 7000);
+//     enterthepin(withdraw)
+// }
+
+// function accountholder(choosetheaccount){
+//     setTimeout(() => {
+//         console.log('welcome bob')
+//     }, 4000);
+//     choosetheaccount(atmpin)
+// }
+
+// mytransaction(accountholder)
 
 
- 
+
+
+//promises
+
+
+
+
+// function mytransaction() {
+//     new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log('your transaction begins here')
+//         }, 1000);
+//           resolve(accountholder);
+//     })
+//     .then((accountholdername)=>{
+//         accountholdername(chooseaccount)
+//     })
+// }
+
+
+// function message(){
+//     new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log('thank you visit again')
+//         }, 11000);
+//         resolve('success')
+//     })
+// }
+
+
+// function withdrawmsg(){
+//     new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log('withdraw is successfull')
+//         }, 9000);
+//         resolve(message)
+//     })
+//     .then((sendoffmsg)=>{
+//         sendoffmsg(message)
+//     })
+    
+// }
+
+// function atmpin(){
+//     new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log('enter the pin')
+//         }, 8000);
+//         resolve(withdrawmsg)
+//     })
+//     .then((withdrawtheamt)=>{
+//         withdrawtheamt(withdrawmsg)
+//     })
+// }
+
+
+// function chooseaccount(){
+//     new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log('sure you wnat to choose the savings account')
+//         }, 6000);
+//         resolve(atmpin)
+//     })
+//     .then((enterthepin)=>{
+//         enterthepin(atmpin)
+//     })
+// }
+
+
+// function accountholder(){
+//     new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log('welcome messi the goat')
+//         }, 4000);
+//         resolve(chooseaccount);
+//     })
+//     .then((choosetheaccount)=>{
+//         choosetheaccount(atmpin)
+//     })
+// }
+
+//  mytransaction(accountholder)
+
+
+
+
+
+// 1) Create 5 Student List Each Student should have results -> tamil,Eng,Math,Science,SS and marks
+
+// a) Sum Total for each Student
+// b) Find Higest and Lowert marks
+// c) Find Failed Student List
+// d) Find Best and Worst scored subject 
+
+const studentList = [
+    {
+        name:'messi',
+        examResults:
+            [
+                {subject:'tamil',marks:88,},
+                {subject:'english',marks:68},
+                {subject:'maths',marks:58},
+                {subject:'science',marks:82},
+                {subject:'socialscience',marks:90}
+            ]
+
+        
+    },
+
+    {
+       name:'ronaldo',
+       examResults:
+        [
+            {subject:'tamil',marks:83},
+            {subject:'english',marks:65},
+            {subject:'maths',marks:38},
+            {subject:'science',marks:86},
+            {subject:'socialscience',marks:100}
+        ]
+
+       
+
+    },
+
+    {
+        name:'bob',
+        examResults:
+            [
+                {subject:'tamil',marks:58},
+                {subject:'english',marks:88},
+                {subject:'maths',marks:28},
+                {subject:'science',marks:92},
+                {subject:'socialscience',marks:92}
+            ]
+        
+    },
+
+    {
+        name:'max',
+        examResults:
+            [
+                {subject:'tamil',marks:86},
+                {subject:'english',marks:98},
+                {subject:'maths',marks:28},
+                {subject:'science',marks:85},
+                {subject:'socialscience',marks:100}
+            ]
+
+        
+    },
+    {
+        name:'jim',
+        examResults:
+            [
+
+                {subject:'tamil',marks:28},
+                {subject:'english',marks:66},
+                {subject:'maths',marks:56},
+                {subject:'science',marks:92},
+                {subject:'socialscience',marks:100}
+            ]
+        
+        }
+    
+
+]
+
+//     studentList.forEach((student)=>{ 
+//       let totalMarks = 0;
+//     student.examResults.forEach((allmarks)=>{
+//         totalMarks += allmarks.marks
+//     })
+//     console.log({name:student.name,total:totalMarks})
+// })
+
+
+// let highestMarks = -Infinity;
+// let lowestMarks = Infinity;
+
+// studentList.forEach((studentMarks)=>{
+//     studentMarks.examResults.forEach((results)=>{
+//         if(results.marks > highestMarks){
+//             highestMarks = results.marks
+//         }
+//         if(results.marks < lowestMarks){
+//             lowestMarks = results.marks
+//         }
+//     })
+//       console.log({name:studentMarks.name,highestMark:highestMarks,lowestMark:lowestMarks})
+// })
+
+
+
+// let failedStudents = 35
+// var failedList =[];
+// studentList.forEach((studentsfailed)=>{
+//     studentsfailed.examResults.forEach((studentMarks)=>{
+//         if(studentMarks.marks < failedStudents){
+//            failedList.push(studentMarks.marks)
+//            console.log({ name:studentsfailed.name,faildMarks:studentMarks.marks})
+//         }
+//     })
+    
+// })
+
+// let bestSocre = -Infinity;
+// let worstSocre = Infinity;
+
+// studentList.forEach((results)=>{
+//     [90,39,45,93,38].
+//     forEach((socre)=>{
+//         console.log(socre)
+//         if(socre.marks > bestSocre){
+//            bestSocre = socre.marks       
+//         }
+//         if(socre.marks < worstSocre){
+//             worstSocre = socre.marks
+//         }
+//     })
+//     console.log({name:results.name,bestSocerdSubject:bestSocre,worstSocredSubject:worstSocre})
+// })
+
+ let bestSocre = -Infinity;
+ let worstSocre = Infinity;
+
+let arr=[90,39,45,93,38]
+  arr.forEach((socre)=>{
+    //  console.log(socre)
+    if(socre > bestSocre){
+        //  console.log(socre)
+        //  console.log(bestSocre)
+       bestSocre = socre 
+      console.log(bestSocre,socre)     
+    }
+    if(socre < worstSocre){
+        //  console.log(worstSocre)
+        worstSocre = socre
+        console.log(worstSocre,socre)
+    }
+})
+
+
+// score 90 bestScore  90 > -In true bestScore 
+
+// in the socre parameter it save all the elemnts from the  variable named arr
+//then it moves the first index into the if condition the bestscore value will be in -infinity
+//in the if condition the score has the value of 90,93 and the bestscore has the value of -infinity and 90
+//in this condition it takes the highest value in our case it takes the value 93 which is higher
+
+// in less than part the score has the value of 90,39,38 and in the worstscore has value of infinity 90,39
+//following the condition it takes the lowest value whuch is 38
+
+
+
+
